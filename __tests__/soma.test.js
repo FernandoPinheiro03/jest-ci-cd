9e9ca49359a30bf61d1c84b2f06f30ada3f78b1f
fernando <fernandopinheiro03@gmail.com>
2023-05-30 20:07:01 -0300

    update name of the tests

--- __tests__/soma.test.js
+++ __tests__/soma.test.js
@@ -1,7 +1,35 @@
 function soma(a, b) {
     return a + b;
   }
+
+  function subtracao(a, b) {
+    return a - b;
+  }
   
-  test('deve retornar a soma de dois números', () => {
+  function multiplicacao(a, b) {
+    return a * b;
+  }
+
+  function divisao(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      return 'Divisão por zero não é definida.';
+    }
+  }
+  
+  test('Teste Soma', () => {
     expect(soma(2, 3)).toBe(5);
+  });
+
+  test('Teste Multiplicacao', () => {
+    expect(multiplicacao(4, 5)).toBe(20);
+  });
+  
+  test('Teste Divisão', () => {
+    expect(divisao(10, 2)).toBe(5);
+  });
+  
+  test('Teste Divisão', () => {
+    expect(divisao(5, 0)).toBe('Divisão por zero não é definida.');
   });
